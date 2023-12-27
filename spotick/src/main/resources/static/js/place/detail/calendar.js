10b44fb7d8d082fb7553b55d55fee28a80d3677e
gimdaeyeon <rlaeodus1306@naver.com>
2023-12-27 16:02:52 +0900

    Design: datepicker 추가

---
+++ spotick/src/main/resources/static/js/place/detail/calendar.js
@@ -0,0 +1,48 @@
+// Datepicker의 기본 설정을 한국어로 변경
+$.datepicker.setDefaults({
+    dateFormat: 'yy-mm-dd',
+    prevText: '이전 달',
+    nextText: '다음 달',
+    monthNames: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+    monthNamesShort: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+    dayNames: ['일', '월', '화', '수', '목', '금', '토'],
+    dayNamesShort: ['일', '월', '화', '수', '목', '금', '토'],
+    dayNamesMin: ['일', '월', '화', '수', '목', '금', '토'],
+    showMonthAfterYear: true,
+    yearSuffix: '년'
+
+});
+
+// 페이지가 로드될 때 실행할 함수
+$(function () {
+    let currentDate = new Date();  // 현재 날짜를 가져옴
+    // datepicker 설정
+    $("#datepicker").datepicker({
+        minDate: currentDate,  // 선택할 수 있는 최소 날짜를 현재 날짜로 설정
+        onSelect: function (dateText) {  // 날짜를 선택했을 때 실행할 함수
+            $("#selectedDate").val(dateText);  // 선택한 날짜를 input 요소에 표시
+        },
+        beforeShowDay: function (date) {  // 달력의 각 날짜를 표시하기 전에 실행할 함수
+            let selectedDate = $("#selectedDate").datepicker('getDate');  // 선택한 날짜를 가져옴
+            // 선택한 날짜와 표시할 날짜가 같으면 'selectedDate' 클래스를 추가
+            if (selectedDate !== null) {
+                if (date.getTime() === selectedDate.getTime()) {
+                    return [true, 'selectedDate'];
+                }
+            }
+            return [true, ''];  // 그렇지 않으면 클래스를 추가하지 않음
+        }
+    });
+});
+
+
+// 현재 날짜를 가져와서 '년-월-일' 형태로 표시
+let today = new Date();  // 현재 날짜를 가져옴
+
+let year = today.getFullYear();  // 년도를 가져옴
+let month = ('0' + (today.getMonth() + 1)).slice(-2);  // 월을 가져옴 (월은 0부터 시작하므로 1을 더하고, 두 자리 수를 유지하기 위해 앞에 0을 추가)
+let day = ('0' + today.getDate()).slice(-2);  // 일을 가져옴 (두 자리 수를 유지하기 위해 앞에 0을 추가)
+
+let dateString = year + '-' + month + '-' + day;  // 날짜를 '년-월-일' 형태로 만듦
+
+$("#selectedDate").val(dateString);  // 만든 날짜를 input 요소에 표시
--- spotick/src/main/resources/static/js/place/detail/detail.js
+++ spotick/src/main/resources/static/js/place/detail/detail.js
@@ -20,6 +20,29 @@ $('.modal-close').on('click',function (){
     $('.modal-container').addClass('none');
 });
 
+let currentIndex = 0; // 현재 활성화된 이미지의 인덱스
+let images = $('.modal-img-item');
+let imageCount = images.length; // 이미지의 총 개수
+let imageWidth = images.width(); // 각 이미지의 너비
+
+// 이전 버튼 클릭 이벤트
+$('.prev').on('click',function() {
+    currentIndex = (currentIndex - 1 + imageCount) % imageCount;
+    updateSliderPosition();
+});
+
+// 다음 버튼 클릭 이벤트
+$('.next').on('click',function() {
+    currentIndex = (currentIndex + 1) % imageCount;
+    updateSliderPosition();
+});
+
+// 슬라이더 위치 업데이트
+function updateSliderPosition() {
+    let translateValue = -currentIndex * imageWidth;
+    $('.modal-img-box').css('transform', 'translateX(' + translateValue + 'px)');
+    $('.current-img-num').text(currentIndex+1);
+}
 
 
 
