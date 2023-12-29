72bbd521ba79a77246c0f53ddb87b908cbe637e7
devnamho0910 <devnamho0910@gmail.com>
2023-12-29 11:45:39 -0500

    Feat: 마이페이지-예약내역 > 결제시스템 추가

---
+++ spotick/src/main/resources/static/js/global-js/bootpay.js
@@ -0,0 +1,30 @@
+function payItem() {
+    const response = Bootpay.requestPayment({
+        "application_id": "59a4d323396fa607cbe75de4",
+        "price": 1000,
+        "order_name": "테스트결제",
+        "order_id": "TEST_ORDER_ID",
+        "pg": "다날",
+        "method": "카드",
+        "tax_free": 0,
+        "user": {
+            "id": "회원아이디",
+            "username": "회원이름",
+            "phone": "01000000000",
+            "email": "test@test.com"
+        },
+        "items": [
+            {
+                "id": "item_id",
+                "name": "테스트아이템",
+                "qty": 1,
+                "price": 1000
+            }
+        ],
+        "extra": {
+            "open_type": "iframe",
+            "card_quota": "0,2,3",
+            "escrow": false
+        }
+    })
+}
