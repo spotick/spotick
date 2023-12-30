135b037ce83e191589d21cb2d8259da063dfcab5
gimdaeyeon <rlaeodus1306@naver.com>
2023-12-30 20:59:00 +0900

    Design: 장소 예약 및 결제화면 완료

---
+++ spotick/src/main/resources/static/js/place/reserve/resereve.js
@@ -0,0 +1,62 @@
+
+let defaultPeople =4;
+
+$('.visitor-ctr-box button').on('click',function (){
+    let $visitors = $('.visitors');
+    let visitNum= Number($visitors.val());
+    let number = visitNum+Number($(this).val());
+    $visitors.val(number>99?99:number);
+    updateSelectedPeople()
+});
+
+$('.plus').on('click',function (){
+    let $visitors = $('.visitors');
+    let visitNum= $visitors.val();
+    if(visitNum>=99){
+        $visitors.val(99);
+        return;
+    }
+    $visitors.val(++visitNum);
+    updateSelectedPeople()
+});
+
+$('.minus').on('click',function (){
+    let $visitors = $('.visitors');
+    let visitNum= $visitors.val();
+    if(visitNum<=1){
+        $visitors.val(1);
+        return;
+    }
+    $visitors.val(--visitNum);
+    updateSelectedPeople()
+});
+
+function updateSelectedPeople(){
+    let selectedCount = $('.visitors').val();
+    let additionMsg = selectedCount>defaultPeople? ` (${selectedCount-defaultPeople}명 추가)`:``;
+
+    $('.selected-count').text(`${selectedCount}명${additionMsg}`);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
