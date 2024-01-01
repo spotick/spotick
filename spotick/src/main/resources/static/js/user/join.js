e08b8b1ac0305a994b1c488e631f050c129ffc42
gimdaeyeon <rlaeodus1306@naver.com>
2024-01-01 18:27:51 +0900

    Refactor: 회원가입 페이지 유효성 검사부분 로직 변경

--- spotick/src/main/resources/static/js/user/join.js
+++ spotick/src/main/resources/static/js/user/join.js
@@ -1,9 +1,4 @@
-/**
- * 서버연결되고 유효성검사로 회원가입 버튼 누루게 하는 부분 수정해야함
- * 지금 상태로 하면 모든 인풋마다 클릭이나 blur이벤트 발생될 때마다
- * 불필요하게 ajax연결이 되서 비효율적임
- * 일단은 퍼블리싱 작업부터
- */
+
 $(".all-check").on("click", function () {
     $(this).toggleClass("on");
 
@@ -30,8 +25,6 @@ $('input').on('focus',function (){
     $(this).closest('.input-box').css('border-color','#dfe2e7');
 })
 
-
-
 function isAllCheck() {
     for (let i = 0; i < $(".check").length; i++) {
         if (!$(".check")[i].classList.contains("on")) {
@@ -41,21 +34,12 @@ function isAllCheck() {
     return true;
 }
 
-// 필수 동의목록 체크 확인
-function requiredCheckOk() {
-    for (let i = 0; i < $(".required").length; i++) {
-        if (!$(".required")[i].classList.contains("on")) {
-            return false;
-        }
-    }
-    return true;
-}
-
 // 인풋 및 체크여부에 따라 서브밋 버튼 동작 설정
-$("body").on("change click",'input, .agreement>div', function () {
-    // 간단한 회원가입시의 체크 검사
-    // $(".submit-btn").toggleClass("on", requiredCheckOk());
-    // 모든 유효성 검사 적용
+$('.agreement>div').on('click',function (){
+    $(".submit-btn").toggleClass("on", isValidFields());
+});
+
+$("input").on('change',function (){
     $(".submit-btn").toggleClass("on", isValidFields());
 });
 
@@ -65,25 +49,22 @@ $(".submit-box").on("click", ".on", function () {
 });
 
 function isValidFields(){
-    return isValidEmail()
-        &&isValidNickName()
-        &&isValidPasswordLength()
-        &&isInputEmpty()
-        &&requiredCheckOk();
+    return
+    isInputOk() &&
+        requiredCheckOk();
 }
 
 
 // 이메일 유효성 및 중복 검사
 function isValidEmail() {
     const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
-    let isValid = regex.test($("#email").val());
+    const $email = $("#email");
+    let isValid = regex.test($email.val());
     let $emailFail = $('.email-fail');
-    $emailFail.text('');
-    if(!isValid){
-        $emailFail.text('유효하지 않은 이메일 형식입니다.');
-    }
 
-    return isValid;
+    $emailFail.text(isValid?'':'유효하지 않은 이메일 형식입니다.');
+
+    $email.toggleClass('ok',isValid);
 
     // $.ajax({
     //     url: ``, //url
@@ -107,7 +88,12 @@ function isValidEmail() {
 
 // 닉네임 중복 검사
 function isValidNickName() {
-    return $("#nick-name").val() !== '';
+    let $nickName =$("#nick-name");
+    let isValid = $nickName.val() !== '';
+    
+    $('.nick-name-fail').text(isValid?'':'닉네임을 다시 입력해주세요');
+
+    $nickName.toggleClass('ok',isValid);
     // $.ajax({
     //     url: ``, //url
     //     type: "post",
@@ -129,25 +115,55 @@ function isValidNickName() {
 }
 
 // 비밀번호 유효성 검사
-function isValidPasswordLength() {
+function isValidPassword() {
     const passwordRegex = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,20}$/;
-    if (passwordRegex.test($("#password").val())) {
-        $(".password-fail").addClass("none");
-        return true;
-    } else {
-        $(".password-fail").removeClass("none");
-        return false;
-    }
+    let $password = $("#password");
+    let isValid = passwordRegex.test($password.val());
+    $(".password-fail").toggleClass('none',isValid);
+    $password.toggleClass('ok',isValid);
+
 }
 
-function isInputEmpty() {
-    let isValid = true;
-    $("input").each(function () {
-        if ($(this).val().trim() === "") {
-            isValid = false;
-            return false; // 반복 중단
-        }
-    });
-    return isValid;
+function isValidTelNumber(){
+    const telRegex = /^01([0|1|6|7|8|9]?)-?([0-9]{3,4})-?([0-9]{4})$/;
+    let $tel = $('#tel');
+    let isValid = telRegex.test($tel.val());
+    $('.tel-fail').text(isValid?'':'올바른 전화번호를 입력해주세요');
+
+    $tel.toggleClass('ok',isValid);
 }
+
+function isInputOk(){
+    let $inputArr = $('input');
+
+    for (let i = 0; i < $inputArr.length; i++) {
+        if (!$inputArr[i].classList.contains("ok")) {
+            return false;
+        }
+    }
+    return true;
+}
+
+// 필수 동의목록 체크 확인
+function requiredCheckOk() {
+    for (let i = 0; i < $(`.required`).length; i++) {
+        if (!$(".required")[i].classList.contains("on")) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
