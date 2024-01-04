65c9ec82c305f43346f22c776d7b706501693821
devnamho0910 <devnamho0910@gmail.com>
2024-01-04 18:08:54 -0500

    Design: MY북마크 페이지 수정

--- spotick/src/main/resources/static/js/mypage/bookmarks.js
+++ spotick/src/main/resources/static/js/mypage/bookmarks.js
@@ -1,4 +1,7 @@
 // 북마크 버튼 컨트롤
-function toggleBookmark(item) {
-    item.classList.toggle('active');
+function toggleBookmark(button, postId) {
+    // todo : postId를 받아 북마크 등록 및 해제하는 비동기
+
+
+    button.classList.toggle('active');
 }
