f14f676da6debe57eab4336bf1c4ec6e2f673b58
devnamho0910 <devnamho0910@gmail.com>
2024-01-09 09:26:09 -0500

    Feat: 메인페이지 경로 재설정

--- src/main/java/com/app/spotick/controller/main/MainController.java
+++ src/main/java/com/app/spotick/controller/main/MainController.java
@@ -6,8 +6,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MainController {
 
+//    상시 메인페이지는 장소리스트페이지로 함
+//    /place/list 요청시에도 같은 페이지가 나오도록 처리 필요
     @RequestMapping
     public String goToMain() {
-        return "place/detail";
+        return "place/list";
     }
 }
