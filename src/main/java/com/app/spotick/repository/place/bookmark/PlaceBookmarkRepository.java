7a10bdf344c8dfe7df254291b5f1f31e1758ccdd
gimdaeyeon <rlaeodus1306@naver.com>
2024-01-16 18:40:04 +0900

    WIP: queryDSL작업중

--- src/main/java/com/app/spotick/repository/place/bookmark/PlaceBookmarkRepository.java
+++ src/main/java/com/app/spotick/repository/place/bookmark/PlaceBookmarkRepository.java
@@ -1,4 +1,4 @@
-package com.app.spotick.repository.place;
+package com.app.spotick.repository.place.bookmark;
 
 import com.app.spotick.domain.entity.place.PlaceBookmark;
 import org.springframework.data.jpa.repository.JpaRepository;
