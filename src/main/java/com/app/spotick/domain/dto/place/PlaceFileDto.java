6e2678f9a698301de1fd4f7cd4c57eb527ad4989
gimdaeyeon <rlaeodus1306@naver.com>
2024-01-17 13:13:04 +0900

    Test: queryDsl테스트

--- src/main/java/com/app/spotick/domain/dto/place/PlaceFileDto.java
+++ src/main/java/com/app/spotick/domain/dto/place/PlaceFileDto.java
@@ -5,11 +5,13 @@ import lombok.NoArgsConstructor;
 
 @Data @NoArgsConstructor
 public class PlaceFileDto {
+    private Long placeFileId;
     private String fileName;
     private String uuid;
     private String uploadPath;
 
-    public PlaceFileDto(String fileName, String uuid, String uploadPath) {
+    public PlaceFileDto(Long placeFileId,String fileName, String uuid, String uploadPath) {
+        this.placeFileId = placeFileId;
         this.fileName = fileName;
         this.uuid = uuid;
         this.uploadPath = uploadPath;
