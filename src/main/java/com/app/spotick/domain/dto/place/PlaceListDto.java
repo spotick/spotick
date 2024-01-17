ca762c6427f0eb6b375eddbecb6c0e0c85d0a0cc
devnamho0910 <devnamho0910@gmail.com>
2024-01-16 19:12:47 -0500

    Fix: 북마크리스트 정보 정상 출력하도록 수정

--- src/main/java/com/app/spotick/domain/dto/place/PlaceListDto.java
+++ src/main/java/com/app/spotick/domain/dto/place/PlaceListDto.java
@@ -1,6 +1,5 @@
 package com.app.spotick.domain.dto.place;
 
-import com.app.spotick.domain.base.image.ImageBase;
 import com.app.spotick.domain.embedded.post.PostAddress;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,14 +18,21 @@ public class PlaceListDto {
     private Long bookmarkCount;
 
 
-    public PlaceListDto(Long id, String title, Integer price, PostAddress placeAddress, List<PlaceFileDto> placeFiles, Double evalAvg, Long evalCount, Long bookmarkCount) {
+    public PlaceListDto(Long id, String title, Integer price, PostAddress placeAddress, Double evalAvg, Long evalCount, Long bookmarkCount) {
         this.id = id;
         this.title = title;
         this.price = price;
         this.placeAddress = placeAddress;
-        this.placeFiles = placeFiles;
         this.evalAvg = evalAvg;
         this.evalCount = evalCount;
         this.bookmarkCount = bookmarkCount;
+    }
+
+    public void updatePlaceFiles(List<PlaceFileDto> placeFiles) {
+        this.placeFiles = placeFiles;
+    }
+
+    public void updateEvalAvg(Double evalAvg) {
+        this.evalAvg = evalAvg;
     }
 }
