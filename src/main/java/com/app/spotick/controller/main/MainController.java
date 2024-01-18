26d20f0fbd4e60d7f10e55d2fb1d3b19887964b7
devnamho0910 <devnamho0910@gmail.com>
2024-01-17 19:16:33 -0500

    Feat: 북마크 기능 구현 및 적용

--- src/main/java/com/app/spotick/controller/main/MainController.java
+++ src/main/java/com/app/spotick/controller/main/MainController.java
@@ -1,30 +1,27 @@
 package com.app.spotick.controller.main;
 
+import com.app.spotick.domain.dto.place.PlaceListDto;
 import com.app.spotick.domain.entity.user.User;
+import com.app.spotick.service.place.PlaceService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class MainController {
+    private final PlaceService placeService;
 
 //    상시 메인페이지는 장소리스트페이지로 함
 //    /place/list 요청시에도 같은 페이지가 나와야 함.
     @RequestMapping
-    public String goToMain(HttpServletRequest request) {
-//        회원가입 및 로그인 기능 추가 이전에 마이페이지에서 사용할 테스트 코드임 삭제하지 말 것
-//        HttpSession session = request.getSession();
-//
-//        User user = User.builder()
-//                .id(1L)
-//                .email("cnh1234578@gmail.com")
-//                .nickName("안녕하세요.")
-//                .build();
-//
-//        // 세션에 사용자 정보 추가
-//        session.setAttribute("user", user);
-
+    public String goToMain(Model model) {
+        Page<PlaceListDto> placeList = placeService.findPlaceListPagination(0);
+        model.addAttribute("placeList", placeList);
         return "place/list";
     }
 }
--- src/main/java/com/app/spotick/service/place/PlaceBookmarkServiceImpl.java
+++ src/main/java/com/app/spotick/service/place/PlaceBookmarkServiceImpl.java
@@ -1,16 +1,66 @@
 package com.app.spotick.service.place;
 
 import com.app.spotick.domain.dto.place.PlaceListDto;
+import com.app.spotick.domain.entity.place.Place;
+import com.app.spotick.domain.entity.place.PlaceBookmark;
+import com.app.spotick.domain.entity.user.User;
+import com.app.spotick.repository.place.PlaceRepository;
 import com.app.spotick.repository.place.bookmark.PlaceBookmarkRepository;
+import com.app.spotick.repository.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class PlaceBookmarkServiceImpl implements PlaceBookmarkService {
     private final PlaceBookmarkRepository placeBookmarkRepository;
+    private final PlaceRepository placeRepository;
+    private final UserRepository userRepository;
+
+    @Override
+    public boolean bookmark(Long placeId, Long userId) {
+        Place place = placeRepository.findById(placeId).orElseThrow(
+                NoSuchElementException::new
+        );
+
+        User user = userRepository.findById(userId).orElseThrow(
+                NoSuchElementException::new
+        );
+
+        Optional<PlaceBookmark> bookmark = placeBookmarkRepository.findByPlaceAndUser(place, user);
+
+        if (bookmark.isEmpty()) {
+            PlaceBookmark placeBookmark = PlaceBookmark.builder()
+                    .place(place)
+                    .user(user)
+                    .build();
+            placeBookmarkRepository.save(placeBookmark);
+            return true;
+        } else {
+            placeBookmarkRepository.delete(bookmark.get());
+            return false;
+        }
+
+    }
+
+    @Override
+    public boolean bookmarkCheck(Long placeId, Long userId) {
+        Place place = placeRepository.findById(placeId).orElseThrow(
+                NoSuchElementException::new
+        );
+
+        User user = userRepository.findById(userId).orElseThrow(
+                NoSuchElementException::new
+        );
+
+        Optional<PlaceBookmark> bookmark = placeBookmarkRepository.findByPlaceAndUser(place, user);
+
+        return bookmark.isPresent();
+    }
 }
