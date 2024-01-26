78ce929b2862e94456489a12ebce9ba1d28b800c
devnamho0910 <devnamho0910@gmail.com>
2024-01-26 17:18:38 -0500

    Perf: 예약내역 수정시 검증코드 최적화

--- src/main/java/com/app/spotick/controller/mypage/MypageController.java
+++ src/main/java/com/app/spotick/controller/mypage/MypageController.java
@@ -182,27 +182,37 @@ public class MypageController {
     @ResponseBody
     public ResponseEntity<String> cancelReservation(@PathVariable("reservationId") Long reservationId,
                                                     @AuthenticationPrincipal UserDetailsDto userDetailsDto) {
+        if (reservationId == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("잘못된 요청입니다.");
+        }
+
         // reservationId검증: userId 동일한지 검증 필요 -> 시간정보 찾아와서 checkIn시간 체크하여 지나지 않았는지 검증 -> accepted 검증(아닐 시 취소 불가능)
         PlaceReservation reservation = placeReservationService
                 .findReservationByIdAndUser(reservationId, userDetailsDto.getId()).orElse(null);
 
-        // if문 여러개보다는 하나하나씩 필터링하여 성능 문제를 최대한 개선하고자 else if로 이어감.
         if (reservation == null) {
             // 예약 정보를 찾을 수 없을 시
             return ResponseEntity.status(HttpStatus.NOT_FOUND)
                     .body("예약 정보를 찾을 수 없습니다.");
 
-        } else if (!Objects.equals(reservation.getUser().getId(), userDetailsDto.getId())) {
+        }
+
+        if (!Objects.equals(reservation.getUser().getId(), userDetailsDto.getId())) {
             // 조회를 하여도 user의 정보가 해당 로그인 유저와 다를 시
             return ResponseEntity.status(HttpStatus.FORBIDDEN)
                     .body("예약 정보에 접근할 권한이 없습니다.");
 
-        } else if (reservation.getCheckIn().isBefore(LocalDateTime.now())) {
+        }
+
+        if (reservation.getCheckIn().isBefore(LocalDateTime.now())) {
             // checkIn시간이 현재 시간보다 이전일 경우, 이미 승인된 예약일 경우
             return ResponseEntity.status(HttpStatus.FORBIDDEN)
                     .body("예약 시간이 지나간 예약은<br>취소할 수 없습니다.");
 
-        } else if (reservation.getReservationStatus().equals(PlaceReservationStatus.APPROVED)) {
+        }
+
+        if (reservation.getReservationStatus().equals(PlaceReservationStatus.APPROVED)) {
             // 승인된 예약은 취소 불가
             return ResponseEntity.status(HttpStatus.FORBIDDEN)
                     .body("이미 승인된 예약은<br>취소할 수 없습니다.");
@@ -219,27 +229,37 @@ public class MypageController {
     public ResponseEntity<String> deleteReservation(@PathVariable("reservationId") Long reservationId,
                                                     @AuthenticationPrincipal UserDetailsDto userDetailsDto,
                                                     RedirectAttributes redirectAttributes) {
+        if (reservationId == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("잘못된 요청입니다.");
+        }
+
         // reservationId검증: userId 동일한지 검증 필요 -> 시간정보 찾아와서 checkOut시간 체크하여 지나갔는지 검증 -> accepted, rejected, cancled아닐 시 삭제 불가.
         PlaceReservation reservation = placeReservationService
                 .findReservationByIdAndUser(reservationId, userDetailsDto.getId()).orElse(null);
 
-        // if문 여러개보다는 하나하나씩 필터링하여 성능 문제를 최대한 개선하고자 else if로 이어감.
         if (reservation == null) {
             // 예약 정보를 찾을 수 없을 시
             return ResponseEntity.status(HttpStatus.NOT_FOUND)
                     .body("예약 정보를 찾을 수 없습니다.");
 
-        } else if (!Objects.equals(reservation.getUser().getId(), userDetailsDto.getId())) {
+        }
+
+        if (!Objects.equals(reservation.getUser().getId(), userDetailsDto.getId())) {
             // 조회를 하여도 user의 정보가 해당 로그인 유저와 다를 시
             return ResponseEntity.status(HttpStatus.FORBIDDEN)
                     .body("예약 정보에 접근할 권한이 없습니다.");
 
-        } else if (reservation.getCheckOut().isAfter(LocalDateTime.now())) {
+        }
+
+        if (reservation.getCheckOut().isAfter(LocalDateTime.now())) {
             // checkIn시간이 현재 시간보다 이전일 경우, 이미 승인된 예약일 경우
             return ResponseEntity.status(HttpStatus.FORBIDDEN)
                     .body("예약시간이 지나지 않은 예약은<br>삭제할 수 없습니다.");
 
-        } else if (reservation.getReservationStatus().equals(PlaceReservationStatus.PENDING)
+        }
+
+        if (reservation.getReservationStatus().equals(PlaceReservationStatus.PENDING)
                 || reservation.getReservationStatus().equals(PlaceReservationStatus.WAITING_PAYMENT)) {
             // 예약이 해지되지 못하고 유효한 상태일 시 삭제불가
             return ResponseEntity.status(HttpStatus.FORBIDDEN)
