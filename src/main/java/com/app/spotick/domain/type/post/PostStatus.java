4dccdbd9300a1b81ed06a62634aa657502c33ba3
gimdaeyeon <rlaeodus1306@naver.com>
2024-02-03 22:43:14 +0900

    Feat: 장소 상세보기 위치에 따른 메뉴 포커스처리, 시큐리티 설정 추가, PostStatus 상태 추가

--- src/main/java/com/app/spotick/domain/type/post/PostStatus.java
+++ src/main/java/com/app/spotick/domain/type/post/PostStatus.java
@@ -8,7 +8,8 @@ public enum PostStatus {
     APPROVED("활성화"),             // 승인
     REJECTED("거절됨"),             // 거절
     REPLACED("대체됨"),             // 대체됨
-    MODIFICATION_PENDING("수정 대기중"),  // 수정 대기중
+    MODIFICATION_PENDING("수정 대기중"),  // 수정 대기중(수정신청을 한 원본 게시글)
+    MODIFICATION_REQUESTED("수정 신청됨"),// 수정 신청됨(수정신청시 등록된 게시글)
     DISABLED("비활성화")    //비활성화(호스트가 임의로 중단할 때)
     ;
 
