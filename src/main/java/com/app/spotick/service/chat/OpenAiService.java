5e8d813142fb086cb5cb679d4ed375626db2061a
devnamho0910 <devnamho0910@gmail.com>
2024-02-25 14:28:56 -0500

    Fix: gpt 파인튜닝 1차

--- src/main/java/com/app/spotick/service/chat/OpenAiService.java
+++ src/main/java/com/app/spotick/service/chat/OpenAiService.java
@@ -2,6 +2,7 @@ package com.app.spotick.service.chat;
 
 import com.app.spotick.vo.GptChatVo;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
@@ -32,13 +33,21 @@ public class OpenAiService {
         GptChatVo system = new GptChatVo();
         system.setRole("system");
         system.setContent("""
-                You are the customer service representing our site. capable of answering anything about our website. The name of our site is Spotick. Our website has features that allow users to rent venues, help organize specific events through our ticketing service, and help promote those events.
+                You are the customer service representing our site. capable of answering anything about our website. The name of our site is "Spotick". Our website has features that allow users to rent venues, help organize specific events through our ticketing service, and help promote those events.
                 If a question comes in about the features or services on our site, based on the previous question, make the user able to use the service right away by providing them with:
                 1. <a href="/place/list" style="font-weight: bolder; font-size: 20px; color: #68a5fe;">장소 서비스</a>\s
                 2. <a href="/ticket/list" style=" font-weight: bolder; font-size: 20px; color: #68a5fe;">티켓팅 서비스</a>\s
                 3. <a href="/promotion/list" style=" font-weight: bolder; font-size: 20px; color: #68a5fe;">홍보 게시판</a>\s
                 In the form of 'a tags', place the service name and url exactly as I showed you in the HTML tags.
-                If a difficult question comes in, or if a user complains, comfort them and provide our company number 111-111-1111 and company email test@gmail.com, asking them to contact us. Please answer in Korean and be concise. However, if you are asked questions not totally related with our website and service, for example Code Review, then tell them, politely and genuinely, you only accept questions about services related to ours.""");
+                If you are asked where to register a rental location or ticketing service, provide the following links:
+                1. <a href="/place/register" style="font-weight: bolder; font-size: 20px; color: #68a5fe;">장소 등록</a>\s
+                2. <a href="/ticket/register" style=" font-weight: bolder; font-size: 20px; color: #68a5fe;">티켓팅 등록</a>\s
+                Let users know that the above services require them to log in and encourage them to do so.
+                If you are asked questions, these are the real services or not, then let them know this whole website is just a test not a realtime service.
+                If a difficult question comes in, or if a user complains, comfort them and provide our company number 111-111-1111 and company email test@gmail.com, asking them to contact us.
+                Please answer in Korean and be concise, represent our name of website always in English though.
+                However, if you are asked questions not totally related with our website and service, for example Code Review, then tell them, politely and genuinely, you only accept questions about services related to ours.
+                """);
         list.add(0, system);
 
         return list;
@@ -54,13 +63,13 @@ public class OpenAiService {
     }
 
 
-    public Mono<Map> getGptMessage(List<GptChatVo> list) {
+    public Mono<Map<String, Object>> getGptMessage(List<GptChatVo> list) {
         Map<String, Object> requestBody = createRequestBody(list);
 
         return webClient.post()
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(BodyInserters.fromValue(requestBody))
                 .retrieve()
-                .bodyToMono(Map.class);
+                .bodyToMono(new ParameterizedTypeReference<>() {});
     }
 }
