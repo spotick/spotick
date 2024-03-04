ca2130965d355344057c8803e090bcb49e9dc7f6
gimdaeyeon <rlaeodus1306@naver.com>
2024-03-04 18:00:01 +0900

    Fix: 장소 리스트 페이지 지역필터 충북~전남 적용 안된 부분 수정

--- src/main/java/com/app/spotick/domain/entity/ticket/TicketOrderDetail.java
+++ src/main/java/com/app/spotick/domain/entity/ticket/TicketOrderDetail.java
@@ -11,14 +11,14 @@ public class TicketOrderDetail extends Period {
     @Id @GeneratedValue(generator = "SEQ_TICKET_ORDER_DETAIL_GENERATOR")
     @Column(name = "TICKET_ORDER_DETAIL_ID")
     private Long id;
-    int quantity;
+    private int quantity;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "TICKET_ORDER_ID")
-    TicketOrder ticketOrder;
+    private TicketOrder ticketOrder;
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "TICKET_GRADE_ID")
-    TicketGrade ticketGrade;
+    private TicketGrade ticketGrade;
 
     @Builder
     public TicketOrderDetail(Long id, int quantity, TicketOrder ticketOrder, TicketGrade ticketGrade) {
