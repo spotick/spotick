e5902a802d212f0205362e3f3421fe65fb7706d6
gimdaeyeon <rlaeodus1306@naver.com>
2024-03-22 17:21:27 +0900

    Fix: 장소, 티켓 수정 승인시 원본의 생성일이 수정될 게시글에 적용되도록 수정

--- src/main/java/com/app/spotick/repository/admin/place/AdminPlaceRepository.java
+++ src/main/java/com/app/spotick/repository/admin/place/AdminPlaceRepository.java
@@ -2,8 +2,18 @@ package com.app.spotick.repository.admin.place;
 
 import com.app.spotick.domain.entity.place.Place;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
 
 @Repository
 public interface AdminPlaceRepository extends JpaRepository<Place, Long>,AdminPlaceQDSLRepository{
+
+    //    장소 수정 승인시 원본의 생성날짜를 바뀔 장소에 적용하기 위해 직접 쿼리를 생성하여 적용시킨다.
+    @Modifying
+    @Query("UPDATE Place p SET p.createdDate = :originalCreatedDate WHERE p.id = :changedPlaceId")
+    void updateCreatedDateWithOriginal(@Param("originalCreatedDate") LocalDateTime originalCreatedDate, @Param("changedPlaceId") Long changedPlaceId);
 }
